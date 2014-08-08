2f62239b9da0d06cbca2c40e010665450c6b16a6
Loic Sander <boitaboulet@hotmail.com>
2014-08-08 18:32:27 +0200

    First commit of compare-glyphs.py

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Font to PDF
-## Script for Robofont & DrawBot
-*Robofont version will only work in Robofont 1.6, currently in beta)
-(If you’re not on the beta, use [DrawBot](http://drawbot.readthedocs.org/en/latest/content/download.html)*
+## Scripts for Robofont & DrawBot
+*DISCLAIMER*
+*Robofont versions of these scripts will only work in Robofont v1.6, currently in beta, If you’re not on the beta, use [DrawBot](http://drawbot.readthedocs.org/en/latest/content/download.html)*
 
 Here’s a series of scripts that allow you to generate PDF files directly from a UFO file, that is, either an open font inside of Robofont, or a .ufo you load in DrawBot.
 
@@ -10,15 +10,15 @@ Here’s a series of scripts that allow you to generate PDF files directly from 
 
 As the name indicates, this script is meant to produce proofing sheets. There’s a handful of parameters which can produce the following results (but not limited to):
 
-+ **Print of listed glyphs**
++ **Print of listed glyphs**:
 You provide a list, or lists (glyphNameSets) of glyphs. To have the script use the lists as reference, you must set useString to False. 
 ![alt tag](http://www.akalollip.com/images/github/font2pdf/makeProofingSheets-1.png)
 
-+ **Print of mixed listed glyphs**
++ **Print of mixed listed glyphs**:
 In this case, the script still takes lists as reference but mixes all glyphs of all lists together recursively. Typically, this is meant to produce a spacing proof.
 ![alt tag](http://www.akalollip.com/images/github/font2pdf/makeProofingSheets-3.png)
 
-+ **Print of a string**
++ **Print of a string**:
 If useString is set to True, the script gets the glyphs to set from a string you provide (textToSet). 
 ![alt tag](http://www.akalollip.com/images/github/font2pdf/makeProofingSheets-2.png)
 
@@ -45,3 +45,34 @@ On each page, the script will also set the name of the typeface and the current 
 
 #### PDF Output
 By Default, the script exports a PDF file of the same filename as the .ufo file. Optionaly, you can define a folder to store the PDF in, relatively to the .ufo’s path (change PDFfolder, ex: PDFfolder= '/PDF/'). The folder has to be there, the script won’t create it for you.
+
+### compare-glyphs.py
+
+This script takes in all given fonts (either a list of paths in DrawBot or takes all open fonts in Robofont) and makes a PDF file showing each glyph for all fonts side by side. It is akin to Ondrej Jób’s great [Font Inspector](http://urtd.net/projects/fontinspector/), only the output isn’t HTML but PDF and it’s not interactive. 
+
+It takes it’s character set reference from the first font it can find. Typically, this is intended for master comparison in an interpolation scheme. Let’s say it’s the use case in which you’ll have no surprises (same family name & same number of characters in all fonts).
+
+The script takes the first found font’s glyphOrder as a reference character set. But you can override it by defining your own list  (see fontKeys variable, line 22/25)
+
+#### Page Layout
+
+By default, the script outputs an A4 PDF. The number of columns is defined by the number of fonts input. The pointSize can be modified, it does not change dynamically based on the number of columns. It’s set to an average value yielding okay results up to 5/6 fonts compared. If you need more, you’ll have to change the pointSize yourself (you should figure it out).
+
+![alt tag](http://www.akalollip.com/images/github/font2pdf/compare-glyphs-1.png)
+![alt tag](http://www.akalollip.com/images/github/font2pdf/compare-glyphs-2.png)
+
+You can customize the way glyphs are displayed to a certain extent. You have access to color definition for the glyph themselves (fill and stroke), the cell containing each glyph and background of the page frame (without margins).
+Moreover, you can activate/deactivate display options:
++ **showGlyphBox**: the glyph’s bounding box and vertical metrics
++ **showMetrics**:  displaying metrics values (sidebearings and width)
++ **showName**: glyph name
++ **showCell**: displaying the glyph cell
+
+#### UFO input in DrawBot
+In DrawBot, the simplest way to get your UFOs in is to drag and drop the bunch of them:
+
+![alt tag](http://www.akalollip.com/images/github/font2pdf/compare-glyphs-3.png)
+![alt tag](http://www.akalollip.com/images/github/font2pdf/compare-glyphs-4.png)
+
+#### PDF Output
+The PDF file will start with a timestamp (year-month-day —> 140809) and contain the name of the family plus the names of the compared styles.
---
+++ Robofont/RF-compare-glyphs.py
@@ -0,0 +1,176 @@
+from drawBot import *
+from vanilla import FloatingWindow, TextBox
+import datetime
+
+def _drawGlyph(glyph):
+    path = glyph.naked().getRepresentation("defconAppKit.NSBezierPath")
+    drawPath(path)
+
+fonts = AllFonts()
+thisFont = CurrentFont()
+
+pageWidth = 595.276
+pageHeight = 841.89
+showGlyphBox = False
+showMetrics = True
+showName = True
+showFrame = True
+showCell = True
+infoFontName = ''
+
+fontKeys = aFont.keys()
+#fontKeys = ['a','b','c']
+
+frameBackgroundColor = (.1, .3, .5, 0)
+frameBorderColor = (1, 1, 1, 0)
+
+glyphFillColor = (0, 0, 0, .25)
+glyphStrokeColor= (0, 0, 0)
+glyphStrokeWidth = 5
+
+cellBackgroundColor = (1, 1, 1, 0)
+cellBorderColor = (0, 0, 0, .25)
+
+glyphNameSize = 8
+glyphNameColor = (0, 0, 0, 1)
+
+metricsPointSize = 5
+metricsFillColor = (0, 0, 0)
+
+# number of columns
+col = len(fonts)
+# size of each glyph
+pointSize = 48
+# page margins: top right bottom left 
+margins = (30, 20, 30, 20)
+cellMargins = (0, 0, 0, 0)
+
+innerWidth = pageWidth - (margins[1]+margins[3])
+innerHeight = pageHeight - (margins[0]+margins[2]) 
+
+cellRatio = 1.2
+xLoc = 0.5
+yLoc = 0.5
+cellWidth = innerWidth/col
+cellHeight = cellWidth * cellRatio
+innerCellWidth = cellWidth - cellMargins[1] - cellMargins[3]
+innerCellHeight = cellHeight - cellMargins[0] - cellMargins[2]
+
+glyphsPerPage = col * int(innerHeight/(cellHeight))
+
+glyphs = []
+
+for glyphName in thisFont.glyphOrder:
+    for aFont in fonts:
+        if glyphName in fontKeys:
+            glyphs.append(aFont[glyphName])
+        else:
+            glyphs.append(aFont['.notdef'])
+
+for i, glyph in enumerate(glyphs): 
+
+    thisFont = glyph.getParent()
+
+    UPM = thisFont.info.unitsPerEm
+    xHeight = thisFont.info.xHeight
+    capHeight = thisFont.info.capHeight
+    ascender = thisFont.info.ascender
+    descender = thisFont.info.descender
+    sc = pointSize / UPM  
+    
+    i = i%glyphsPerPage
+    
+    if i == 0:
+        newPage(pageWidth, pageHeight)
+        
+        if showFrame:
+            ## page frame
+            stroke(*frameBorderColor)
+            fill(*frameBackgroundColor)
+            rect(margins[3], margins[2], innerWidth, innerHeight)
+
+        # defining margins and starting from top of the page
+        translate(margins[3], margins[2]+innerHeight)
+    
+    save()
+    
+    lineCount = int(i/col)
+    xPos = cellWidth*(i%col)
+    yPos = cellHeight*(lineCount+1)
+    
+    if showCell:
+        ## tracing glyph cell
+        save()
+        stroke(*cellBorderColor)
+        fill(*cellBackgroundColor)
+        strokeWidth(.25)
+        rect(xPos+cellMargins[3], -yPos+cellMargins[0], innerCellWidth, innerCellHeight)   
+        restore()
+    
+    ### glyph position in table
+    translate(xPos+cellMargins[3]+((innerCellWidth*xLoc)-((glyph.width*xLoc)*sc)), -yPos+cellMargins[2]+((innerCellHeight*(1-yLoc))-(UPM*(1-yLoc)*sc)))
+    
+    ## drawing glyph
+
+    ### shifting inside glyph box
+    scale(sc)
+    translate(0, -descender)
+    
+    if showGlyphBox:
+        save()
+        # bounding box
+        stroke(0.75)
+        strokeWidth(1)        
+        rect(0, descender, glyph.width, UPM)
+        # baseline
+        stroke(0.5, 0.5, 0.75)
+        strokeWidth(3)
+        line((0, 0), (glyph.width,0))
+        # xHeight
+        stroke(0.75, 0.5, 0.5)
+        strokeWidth(3)
+        line((0, xHeight), (glyph.width, xHeight)) 
+        # capHeight
+        stroke(0.5, 0.75, 0.5)
+        strokeWidth(3)
+        line((0, capHeight), (glyph.width, capHeight)) 
+        restore()
+    
+    stroke(*glyphStrokeColor)
+    strokeWidth(glyphStrokeWidth)
+    fill(*glyphFillColor)
+    _drawGlyph(glyph)
+    
+    if showMetrics:
+        save()
+        scale(1/sc)
+        stroke()
+        fill(*metricsFillColor)
+        font(infoFontName)
+        fontSize(metricsPointSize)
+        surrounding = ((innerCellWidth*xLoc)-((glyph.width*xLoc)*sc))
+        textBox(str(int(glyph.leftMargin)), (-surrounding, (xHeight*sc)/2, surrounding, metricsPointSize), 'center')
+        textBox(str(int(glyph.rightMargin)), (glyph.width*sc, (xHeight*sc)/2, surrounding, metricsPointSize), 'center')
+        textBox(str(int(glyph.width)), (0, ((descender-100)*sc)-metricsPointSize, glyph.width*sc, metricsPointSize), 'center')
+        restore()
+    
+    restore()
+    
+    if showName:
+        save()
+        translate(xPos, -yPos)
+        fill(*glyphNameColor)
+        font(infoFontName)
+        fontSize(glyphNameSize)
+        textBox(glyph.name.upper(), (0, cellMargins[2]+glyphNameSize, cellWidth, glyphNameSize), 'center')
+        restore()
+
+faceName = thisFont.info.familyName
+styleNames = '-'.join([aFont.info.styleName for aFont in fonts])
+ufoPath = thisFont.path
+ufoFileName = ufoPath.split('/')[-1]
+ufoFolder = ufoPath.rstrip(ufoFileName)
+timestamp = datetime.datetime.now().strftime('%y%m%d')
+PDFFileName = ufoFolder + '/' + timestamp + '_' + faceName + '_' + styleNames + '.pdf'
+
+saveImage(PDFFileName)
